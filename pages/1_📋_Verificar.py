4398055b0aa8e3ed30c0f91dae47980ac0a6e86f
wlacraf <160874684+wlacraf@users.noreply.github.com>
2025-01-26 09:10:10 -0300

    Update 1_📋_Verificar.py

--- pages/1_📋_Verificar.py
+++ pages/1_📋_Verificar.py
@@ -1,10 +1,10 @@
-import streamlit as st
+import base64
+import folium.plugins
 import pandas as pd
+import streamlit as st
 from streamlit_folium import folium_static
-import folium
 
 
-# Função para carregar dados do CSV
 # Função para carregar dados do CSV e filtrar
 def load_data(file_path):
     # Carrega o DataFrame do arquivo CSV
@@ -19,16 +19,18 @@ def load_data(file_path):
 st.subheader("Pontos a serem verificados")
 file_path = 'pages/piscinas.csv'
 df = load_data(file_path)
-numero=len(df)
+numero = len(df)
 st.write(f"{numero} pontos")
+
 if not df.empty:
-    # Inicia o mapa
-    #m = folium.Map(location=[df['LAT'].mean(), df['LON'].mean()], zoom_start=16)
-    m = folium.Map(location=[ -22.973356,-42.025602], zoom_start=16)
+    # Inicia o mapa na coordenada fixa
+    m = folium.Map(location=[-22.973356, -42.025602], zoom_start=16)
+
+    # Adicionando o plugin LocateControl para mostrar a localização atual
+    folium.plugins.LocateControl(keepCurrentZoomLevel=True, drawMarker=True).add_to(m)
 
     # Adicionando um tileset padrão e alternativos
     folium.TileLayer('OpenStreetMap').add_to(m)
-
 
     # Adicionando Esri Satellite com URL específica
     esri_satellite_url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
@@ -42,18 +44,39 @@ if not df.empty:
 
     folium.LayerControl().add_to(m)  # Permite ao usuário escolher entre os estilos de mapa
 
-    # Adicionando marcadores com popup
-    # Adicionando marcadores com popup
+    # Adicionando marcadores com popup e imagens
     for idx, row in df.iterrows():
         icon_color = 'green' if row['status'] else 'blue'
-        folium.Marker(location=[row['LAT'], row['LON']], popup=f"ID: {row['id_campo']}",stream=f"ID: {row['id_campo']}",
-                      icon=folium.Icon(color=icon_color)).add_to(m)
+
+        # Construir o caminho da imagem usando o id_campo
+        image_path = f"imagens/{row['id_campo']}.jpg"
+
+        try:
+            # Codificar a imagem em base64
+            encoded_image = base64.b64encode(open(image_path, 'rb').read()).decode()
+            image_html = f"<img src='data:image/jpeg;base64,{encoded_image}' width='200' height='200'><br>ID: {row['id_campo']}"
+            iframe = folium.IFrame(image_html, width=220, height=220)
+            popup = folium.Popup(iframe, max_width=2650)
+        except FileNotFoundError:
+            # Se a imagem não for encontrada, apenas exibir o ID
+            popup = folium.Popup(f"ID: {row['id_campo']}", max_width=2650)
+
+        folium.Marker(
+            location=[row['LAT'], row['LON']],
+            popup=popup,
+            tooltip=f"ID: {row['id_campo']}",
+            icon=folium.Icon(color=icon_color)
+        ).add_to(m)
+
     with st.container():
         # Opção para salvar o DataFrame atualizado de volta para um arquivo CSV
         if st.button('Atualizar Alterações'):
             for idx, row in df.iterrows():
                 icon_color = 'green' if row['status'] else 'blue'
-                folium.Marker(location=[row['LAT'], row['LON']], popup=f"ID: {row['id_campo']}",
-                              icon=folium.Icon(color=icon_color)).add_to(m)
+                folium.Marker(
+                    location=[row['LAT'], row['LON']],
+                    popup=f"ID: {row['id_campo']}",
+                    icon=folium.Icon(color=icon_color)
+                ).add_to(m)
 
-folium_static(m)
+    folium_static(m)
